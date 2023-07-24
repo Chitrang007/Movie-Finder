71bf0cdcac6f8ba564b6c61a481f5693827f15ce
Chitrang Chauhan <98351473+Chitrang007@users.noreply.github.com>
2023-07-24 16:30:00 +0530

    Add files via upload
    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { SearchBar } from './components/SearchBar';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header"> What's that movie where...</div>
+      <div className="Search-Bar">
+        <SearchBar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
