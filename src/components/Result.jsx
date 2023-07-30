8062f957ccd6b6584adbec9b8180a65f6fbb5875
Chitrang007 <chauhanchitrang2001@gmail.com>
2023-07-30 19:41:31 +0530

    new diff

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,33 +1,42 @@
 import React from 'react';
 import './Result.css';
 
-export const Result = () => {
+export const Result = (props) => {
+
+    const {movies} = props;
+
+    const boxes = movies.map(
+        (movie) => {
+            return <Box key={movie.id} movie={movie}/>
+        }
+    )
+
     return (
         <div className='res'>
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
+            {boxes}
         </div>
     )
 }
 
-const Box = () => {
+const Box = (props) => {
+    
+    const {movie} = props;
+
     return (
         <div className='Box'>
             <div className='items'>
-                <span style={{display: 'flex',  flex: '1', fontSize: 18}}>Title Here</span>
-                <span className='rating'>9.0<br/></span>
+                <span style={{display: 'flex',  flex: '1'}}>{movie.title}</span>
+                <span className='rating'>{movie.rating}<br/></span>
             </div>
-            <div><br/>Year | Country | Lang</div>
-            <div style={{fontSize: 15}}>
+            <div className='Additional'>
                 <br/>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam"
+                {movie.year} | {movie.country} | {movie.lang}
             </div>
-            <img src="https://picsum.photos/200/300?random=1"/>
+            <div className='plot'>
+                <br/>
+                {movie.plot}
+            </div>
+            <img src="https://picsum.photos/200/300?random=1" alt="" style={{paddingTop: '10px'}}/>
         </div>
     )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import './SearchBar.css';
+import { Result } from './Result';
+
 
 export const SearchBar = () => {
+    
+    const [plot, setPlot] = useState('');
+    const [movies, setMovies] = useState([]);
+
+    const changePlot = (e) => {
+        setPlot(e.target.value);
+    }
+
+    const handleSearch = () => {
+
+        const staticMovieData = [
+            { id: 1, title: 'Iron Man', rating: 7.9, plot: 'Plot of Movie 1', year: 2008, country: 'USA', lang: 'English' },
+            { id: 2, title: 'The Island of Dr. More', rating: 5.8, plot: 'Plot of Movie 2', year: 1977, country: 'USA', lang: 'English' },
+            { id: 3, title: 'The Invincible Iron Man', rating: 6, plot: 'Plot of Movie 3', year: 2007, country: 'USA', lang: 'English' },
+            { id: 4, title: 'Dark Man', rating: 6.4, plot: 'Plot of Movie 4', year: 1990, country: 'USA', lang: 'English' },
+            { id: 5, title: '7 Aum Arivu', rating: 5.8, plot: 'Plot of Movie 5', year: 2011, country: 'India', lang: 'Tamil' },
+        ];
+        setMovies(staticMovieData);
+    }
+
+    useEffect(
+        () => {
+            console.log("Hello");
+        },
+        [plot]
+    )
     return (
-        <div className="input-wrapper">
-            <input className='input' placeholder="Enter plot here..."/>
-            <button className="search-button">Search</button>
+        <div>
+            <div className="input-wrapper">
+                <input 
+                className='input' 
+                type='search'
+                value={plot} 
+                onChange={changePlot} 
+                placeholder="Enter plot here..."/>
+                <input 
+                type='button'
+                className="search-button"
+                value="Search"
+                onClick={handleSearch}/>
+            </div>
+            <Result movies={movies} />
         </div>
     );
 }
